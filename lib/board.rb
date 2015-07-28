a100cc06001de3f5dec5728e7814859d7f272908
Josh <Tattsy@hotmail.co.uk>
2015-07-28 16:28:50 +0100

    we can now fire at the board

--- lib/board.rb
+++ lib/board.rb
@@ -2,14 +2,21 @@ require_relative 'ship'
 class Board
   def initialize(_ships = 0)
     @board_size = [10,10]
-    @board = [[]]
+    @board = Array.new(@board_size[1]) { Array.new(@board_size[0]) }
+    @fired = Array.new(@board_size[1]) { Array.new(@board_size[0]) {false} }
   end
 
-  attr_reader :board
+  attr_reader :board, :fired
 
   def place(ship, x, y)
     fail "Placement is outside board coordiantes" if (1 > x || x > @board_size[0]) || (1 > y || y > @board_size[1])
-    fail "Ship exists at that location!" if @board[y-1][x-1] != nil
-    @board[y-1][x-1] = ship
+    fail "Ship exists at that location!" if board[y-1][x-1] != nil
+    board[y-1][x-1] = ship
+  end
+
+  def fire(x,y)
+  	fail "You fired outside board coordiantes" if (1 > x || x > @board_size[0]) || (1 > y || y > @board_size[1])
+    fail "You have already fired at this location" if fired[y-1][x-1] == true
+    fired[y-1][x-1] = true
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,6 +15,19 @@ describe Board do
       subject.place(ship,1,1)
       expect{subject.place(ship,1,1)}.to raise_error "Ship exists at that location!"
     end
-
+  end
+  describe "#fire" do
+  	it { is_expected.to respond_to(:fire).with(2).argument }
+    it "Should fire at the board" do
+      subject.fire(1,1)
+      expect(subject.fired[0][0]).to eq true
+    end
+    it "Should raise error if fired outside of the board" do
+      expect {subject.fire(0,0)}.to raise_error "You fired outside board coordiantes"
+    end
+    it "Should raise error if fired on previously fired location" do
+      subject.fire(10,10)
+      expect{subject.fire(10,10)}.to raise_error "You have already fired at this location"
+    end
   end
 end
